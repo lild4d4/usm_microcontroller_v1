b396dabafc431bb693cde9b6d1a4a1eadc20f529
lild4d4 <64666124+lild4d4@users.noreply.github.com>
2022-05-01 16:54:47 -0400

    update

---
+++ Hex_programs/C programs/DisplaySwitches.c
@@ -0,0 +1,20 @@
+// memory-mapped I/O addresses
+#define GPIO_SWs    0x00000004
+#define GPIO_LEDs   0x00000008
+
+#define READ_GPIO(dir) (*(volatile unsigned *)dir)
+#define WRITE_GPIO(dir, value) { (*(volatile unsigned *)dir) = (value); }
+
+int main ( void )
+{
+  int En_Value=0xFFFF, switches_value;
+
+  while (1) { 
+    switches_value = READ_GPIO(GPIO_SWs);   // read value on switches
+    switches_value = switches_value >> 16;  // shift into lower 16 bits
+    WRITE_GPIO(GPIO_LEDs, switches_value);  // display switch value on LEDs
+  }
+
+  return(0);
+}
+
